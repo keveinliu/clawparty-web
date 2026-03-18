4e0afe6895fc84707d34ac5ca7f62e065ec76f04
Kevein Liu <kevein@flomesh.cn>
2026-03-18 18:48:26 +0800

    fix: catch unhandledRejection and uncaughtException to prevent process crash

--- src/server.js
+++ src/server.js
@@ -1,10 +1,19 @@
 const { createApp } = require("./app");
 const { getEnv } = require("./config/env");
 const { Scheduler } = require("./scheduler");
+const { logError } = require("./utils/logger");
 
 const env = getEnv();
 const app = createApp();
 const scheduler = new Scheduler();
+
+process.on("unhandledRejection", (reason, promise) => {
+  logError("process.unhandledRejection", reason instanceof Error ? reason : new Error(String(reason)));
+});
+
+process.on("uncaughtException", (err) => {
+  logError("process.uncaughtException", err);
+});
 
 const server = app.listen(env.port, () => {
   console.log(`Server running on http://localhost:${env.port}`);
